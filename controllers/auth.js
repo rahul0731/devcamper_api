07d79be3dab2b7dc8bddb1a21ed30340b78cc266
rahul07 <rahul.yadav@digitalshark.in>
2020-09-25 11:43:52 +0530

    Adding Forgot Password using token

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,3 +85,25 @@ exports.getMe = asyncHandler(async(req,res,next) => {
         data : user
     })
 })
+
+
+//@desc   Forget Password
+//@route  POST /api/v1/auth/forgetpassword
+//@access Private
+
+exports.forgetPassword = asyncHandler(async(req,res,next) => {
+    const user = await User.findOne({email : req.body.email});
+    if(!user){
+        return next(new ErrorResponse('There is no user with email',404));
+    }
+
+    //Get reset token
+    const resetToken = user.getResetPasswordToken();
+   
+    await user.save({validateBeforeSave :false});
+    console.log(resetToken);
+    res.status(200).json({
+        success : true,
+        data : user
+    })
+})
