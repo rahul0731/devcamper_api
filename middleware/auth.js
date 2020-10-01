601fffa5207b613d69cbc6b18081456051bac82b
rahul07 <rahul.yadav@digitalshark.in>
2020-10-01 17:56:48 +0530

    Adding Logout functionality

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,15 +10,17 @@ exports.protect = asyncHandler(async (req,res,next) =>{
         req.headers.authorization && 
         req.headers.authorization.startsWith('Bearer')
      ){
+         //Set token from Bearer token in header
             token = req.headers.authorization.split(' ')[1];
      }
+    //  //Set token from cookie
     //  else if(req.cookies.token){
     //      token = req.cookies.token
     //  }
 
     //Make Sure token exists
     if(!token){
-        return next(new ErrorReponse('Not authorize to access this route' , 401));
+        return next(new ErrorReponse('Not authorized to access this route' , 401));
     }
 
     try {
@@ -28,7 +30,7 @@ exports.protect = asyncHandler(async (req,res,next) =>{
        req.user = await User.findById(decoded.id);
        next();
     } catch (error) {
-        return next(new ErrorReponse('Not authorize to access this route' , 401));
+        return next(new ErrorReponse('Not authorized to access this route' , 401));
     }
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register ,login ,getMe,forgetPassword,resetpassword,updateDetails,updatePassword} = require('../controllers/auth');
+const { register ,login ,getMe,forgetPassword,resetpassword,updateDetails,updatePassword,logout} = require('../controllers/auth');
 
 const router = express.Router();
 const {protect} = require('../middleware/auth')
 router.post('/register',register);
 router.post('/login',login);
+router.get('/logout',logout);
 router.get('/me',protect,getMe);
 router.post('/forgetPassword',forgetPassword);
 router.put('/resetpassword/:resettoken',resetpassword);
